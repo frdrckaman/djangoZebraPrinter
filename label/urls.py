b7389866dda9789cf50a7352aa1e0d576f83930f
frdrckaman <frdrckaman@gmail.com>
2021-11-11 20:56:37 +0300

    print EA

--- label/urls.py
+++ label/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from label.views import HomeView
+from label.views import HomeView, HouseholdLabelView, EALabelView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="labelprint"),
+    path('printByHousehold/', HouseholdLabelView.as_view(), name="byHousehold"),
+    path('printByEA/', EALabelView.as_view(), name="byEA"),
 ]
--- label/views.py
+++ label/views.py
@@ -3,7 +3,30 @@ from django.views.generic.base import TemplateView
 
 
 class HomeView(TemplateView):
+    template_name = f"label/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            third_party_packages=["not available"],
+            installed_apps=settings.INSTALLED_APPS,
+        )
+        return context
+
+
+class HouseholdLabelView(TemplateView):
     template_name = f"label/labelprint.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            third_party_packages=["not available"],
+            installed_apps=settings.INSTALLED_APPS,
+        )
+        return context
+
+class EALabelView(TemplateView):
+    template_name = f"label/labelprint2.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
